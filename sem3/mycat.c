57060b585dd5a0a64245914b993a574e25b56da7
MrYaros <molokan.yaroslavik@gmail.com>
2021-09-18 14:30:48 +0300

    Comments fix

--- sem3/mycat.c
+++ sem3/mycat.c
@@ -1,3 +1,7 @@
+   //////////////////////////////////
+  //            MY CAT            //
+ //          VERSION 1.0         //
+//////////////////////////////////
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -6,32 +10,46 @@
 #include <unistd.h>
 #include <string.h>
 
+//Size of buffer
 #define MAX_SIZE 1024
 
+//Function to write all symbols from buffer to output
+//fd - file desctriptor
+//buffer - pointer on array of chars
+//read_size - num of chars that we have succesfully read
 int mywrite(const int fd, char* buffer, int read_size)
 {
-	ssize_t left_size = 0;
+	//output_size - num of chars that we have in output
+	ssize_t output_size = 0;
 	do
 	{
-		ssize_t write_size = write(1, buffer, read_size - left_size);
+		//write_size - num of chars that we succesfully wrote
+		ssize_t write_size = write(1, buffer, read_size - output_size);
+		//Write check
 		if(write_size < 0)
 		{
 			perror("Write Error");
 			return -1;
 		}
+		//offset on write_size symbols
 		buffer += write_size;
-		left_size += write_size;
+		//num of symbols that we have in the output
+		output_size += write_size;
 	} while(read_size - left_size != 0);
 	return 0;
 }
 
+//Function to call mywrite and read
+//fd - file descriptor
 int cat(const int fd)
 {
-	ssize_t read_size = 0;
 	do
 	{
+		//buffer - array of chars that we have read from fd
 		char buffer[MAX_SIZE];
-		read_size = read(fd, buffer, MAX_SIZE);
+		//read_size - num of chars that we succesfully read
+		ssize_t read_size = read(fd, buffer, MAX_SIZE);
+		//Read check
 		if(read_size < 0)
 		{
 			perror("Read Error");
@@ -45,12 +63,16 @@ int cat(const int fd)
 
 int main(int argc, char const *argv[])
 {
+	//fd - file descriptor
 	int fd = 0;
+	//Mode selection
 	if(argc > 1)
 	{
 		for(int i = 1; i < argc; i++)
 		{
+			//open file and get his fd
 			fd = open(argv[i], O_RDONLY);
+			//Open check
 			if(fd < 0)
 			{
 				perror(argv[i]);
@@ -58,6 +80,7 @@ int main(int argc, char const *argv[])
 			}
 			if(cat(fd) < 0)
 				return -1;
+			//Close check
 			if(close(fd) < 0)
 			{
 				perror("Close Error");
